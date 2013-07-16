e43ef5a3ec357985d7c0994b398a6d8396843caf
cfvalente <caio99valente@hotmail.com>
2013-07-16 19:16:59 -0300

    phong ok

--- OpenGL/fshader_phong.glsl
+++ OpenGL/fshader_phong.glsl
@@ -3,6 +3,7 @@
 in vec4 modelPosition;
 in vec3 normal;
 in vec3 direction;
+in vec3 reflected;
 
 uniform vec3 La;
 uniform vec3 Ld;
@@ -27,13 +28,17 @@ void main ()
 	vec3 n = normalize(normal);
 	vec3 s = normalize(direction);
 	float light_object_ang = max(dot(s,n),0.0);
-
+	vec3 r = normalize(reflected);
+	vec3 camera = vec3(-modelPosition);
+	float camera_reflected_ang = max(dot(r,camera), 0.0);
 
 	diffuse = vec4(Ld*Kd*light_object_ang,1.0);
 
 	ambient = vec4(La*Ka,1.0);
 
-	specular = vec4(0.0,0.0,0.0,1.0);
+	specular = vec4(0.0, 0.0, 0.0, 1.0);
+	if(light_object_ang > 0.0)
+		specular = clamp(vec4(Ls,1.0) * vec4(Ks,1.0) * pow(camera_reflected_ang, Shininess),0,1);
 
 	if(renderingMode == 1) FragColor = diffuse;
 	else if(renderingMode == 2) FragColor = ambient;
--- OpenGL/vshader_phong.glsl
+++ OpenGL/vshader_phong.glsl
@@ -13,16 +13,17 @@ uniform vec4 LightPosition;
 out vec4 modelPosition;
 out vec3 normal;
 out vec3 direction;
-
+out vec3 camera;
+out vec3 reflected;
 
 /* Phong --> Interpolacao de cores em cada fragmento  -- modelo de reflexao phong */
 void main() 
 {
 	normal = vec3(ModelView*vec4(VertexNormal,0.0));  
 	modelPosition = ModelView*vec4(VertexPosition,1.0f); 
-	direction = normalize(vec3(ModelView*LightPosition-modelPosition));
-	//vec3 camera = normalize(-model.xyz);
-	//vec3 reflected = reflect(-direction,tnorm);
+	direction = vec3(ModelView*LightPosition-modelPosition);
+	camera = -modelPosition.xyz;
+	reflected = reflect(-direction,normal);
 	//float camera_reflected_ang = max(dot(reflected,camera), 0.0);
 	gl_Position = MVP*vec4(VertexPosition[0],VertexPosition[1],VertexPosition[2],1.0);
 }
